fe5503c6cea3c6f48fc63ddc50faaf1038e6ec0a
Daramiry <drz0224@gmail.com>
2022-08-14 12:27:41 -0700

    final commit

--- vehicle.js
+++ vehicle.js
@@ -1,3 +1,4 @@
+
 class Vehicle {
     constructor(make, model, year, color, mileage) {
         this.make = make;
@@ -9,9 +10,17 @@ class Vehicle {
         this.mileage = mileage;
         this.started = false;
         this.numberOfWheels = 0;
-        this.fuel = 100
     }
 
+    start() {
+        if (this.fuel > 0) {
+            return this.started = true;
+            console.log("engine started...!!!");
+        } else {
+            return this.started = false;
+            console.log("engine cannot start...");
+        }
+    }
     accelerate() {
         if (this.started) {
             if (this.fuel > 0) {
@@ -25,7 +34,6 @@ class Vehicle {
             alert("You need to start the engine first.");
         }
     }
-
     decelerate() {
         if (this.started) {
             if (this.fuel > 0) {
@@ -44,18 +52,33 @@ class Vehicle {
             alert("You need to start the engine first.");
         }
     }
-
     stop() {
-        console.log('engine off')
         this.started = false;
     }
 
-    typeOfVehicle() {
-        if (this.numberOfWheels == 8) {
+    drive() {
+        accelerate();
+    }
+    brake() {
+        decelerate();
+    }
+
+    autoPark()
+    {
+
+    }
+
+    autoDrive()
+    {
+      
+    }
+
+    typeOfVehicle(wheels) {
+        if (this.numberOfWheels == 8 && 8 == wheels) {
             console.log(this.model + " " + this.make + " is a Truck");
-        } else if (this.numberOfWheels == 4) {
-            console.log(this.model + " " + this.make + " is a Car");
-        } else if (this.numberOfWheels == 2) {
+        } else if (this.numberOfWheels == 4 && 4 == wheels) {
+            console.log(this.model + " " + this.make + " is a CAr");
+        } else if (this.numberOfWheels == 2 && 2 == wheels) {
             console.log(this.model + " " + this.make + " is a Bike");
         } else {
             console.log("Unknown type of vehicle");
@@ -64,4 +87,6 @@ class Vehicle {
 }
 
 //This exports things you want to use from this "module", more info in readme
-// module.exports = { Vehicle }
+module.exports = {
+    Vehicle
+}
